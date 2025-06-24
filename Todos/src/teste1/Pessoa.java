6167a570ef24ce8520d99b27b37764253f442836
Gustavo cardoso guaresi <geraisusos25@gmail.com>
2025-06-23 21:20:26 -0300

    Add files via upload

---
+++ Todos/src/teste1/Pessoa.java
@@ -0,0 +1,14 @@
+package teste1;
+
+public class Pessoa {
+	private String nome;
+	public String getNome() {
+		return nome;
+		
+	}
+	
+	public void setNome(String novonome) {
+		nome = novonome;
+	}
+}
+
